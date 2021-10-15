cc45bcf3a7d607355a8323e09d1566cb4d3185d8
BenjaminLu98 <luzehao1998@gmail.com>
2021-10-15 15:27:17 -0700

    Set Rainbow Color by adding vertex attribute

--- OpenGLFirstProject/a.cpp
+++ OpenGLFirstProject/a.cpp
@@ -12,15 +12,19 @@ const unsigned int SCR_HEIGHT = 600;
 
 const char* vertexShaderSource = "#version 330 core\n"
 "layout (location = 0) in vec3 aPos;\n"
+"layout (location = 1) in vec3 aColor;\n"
+"out vec3 ourColor;"
 "void main()\n"
 "{\n"
 "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"   ourColor=aColor;\n"
 "}\0";
 const char* fragmentShaderSource = "#version 330 core\n"
+"in vec3 ourColor;"
 "out vec4 FragColor;\n"
 "void main()\n"
 "{\n"
-"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"   FragColor = vec4(ourColor, 1.0f);\n"
 "}\n\0";
 
 int main()
@@ -100,10 +104,10 @@ int main()
     // set up vertex data (and buffer(s)) and configure vertex attributes
     // ------------------------------------------------------------------
     float vertices[] = {
-        -0.5f, -0.5f, 0.0f, // left  
-         0.5f, -0.5f, 0.0f, // right 
-        -0.5f,  0.5f, 0.0f, // top left
-         0.5f,  0.5f, 0.0f  // top right   
+        -0.5f, -0.5f, 0.0f,0.0f,1.0f,0.0f, // left  
+         0.5f, -0.5f, 0.0f,1.0f,0.0f,0.0f, // right 
+        -0.5f,  0.5f, 0.0f,0.0f,0.0f,1.0f, // top left
+         0.5f,  0.5f, 0.0f,1.0f,1.0f,1.0f  // top right   
 
     };
 
@@ -125,8 +129,10 @@ int main()
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3*sizeof(float)));
     glEnableVertexAttribArray(0);
+    glEnableVertexAttribArray(1);
 
     // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
     glBindBuffer(GL_ARRAY_BUFFER, 0);
