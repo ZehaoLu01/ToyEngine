ede177ec41f1ac03708a9b3ac6cb2ecf2cbd3798
BenjaminLu98 <luzehao1998@gmail.com>
2021-10-15 20:07:34 -0700

    Texture added.

--- OpenGLFirstProject/fs.glsl
+++ OpenGLFirstProject/fs.glsl
@@ -1,7 +1,9 @@
 #version 330 core
 in vec3 ourColor;
+in vec2 textureCoord;
+uniform sampler2D texturePic;
 out vec4 FragColor;
 void main()
 {
-   FragColor = vec4(ourColor, 1.0f);
+   FragColor =mix(vec4(ourColor,1.0),texture(texturePic,textureCoord),0.2);
 }
