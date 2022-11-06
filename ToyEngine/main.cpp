c4e4b319125b4f459472d74bf9831ea0b19f8551
Zehao Lu <jeffery.lu@blackbirdinteractive.com>
2022-11-05 23:57:58 -0700

    Remove trivial classes

--- ToyEngine/main.cpp
+++ ToyEngine/main.cpp
@@ -3,8 +3,6 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include "Renderer/Shader.h"
-#include "Renderer/VBO.h"
-#include "Renderer/EBO.h"
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
