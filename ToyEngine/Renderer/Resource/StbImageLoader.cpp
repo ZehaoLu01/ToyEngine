75604453266a32646f3771eda73c178792e1dcb3
Zehao Lu <luzehao1998@gmail.com>
2023-04-16 21:20:28 -0700

    change const variable name

--- ToyEngine/Renderer/Resource/StbImageLoader.cpp
+++ ToyEngine/Renderer/Resource/StbImageLoader.cpp
@@ -4,9 +4,9 @@
 
 unsigned char* ToyEngine::StbImageLoader::getImageFrom(std::string path, int* width, int* height, int* channelNum)
 {
-    const std::string defaultPathPrefix = "Resources/Images/";
+    const std::string DEFAULT_PATH_PREFIX = "Resources/Images/";
     stbi_set_flip_vertically_on_load(true);
-    unsigned char* data = stbi_load((defaultPathPrefix + path).c_str(), width, height, channelNum, 0);
+    unsigned char* data = stbi_load((DEFAULT_PATH_PREFIX + path).c_str(), width, height, channelNum, 0);
 
     if (!data) {
         std::cerr << "Resource::StbImageLoader: Image data is not properly loaded. The path is: " << path << "." << std::endl;
