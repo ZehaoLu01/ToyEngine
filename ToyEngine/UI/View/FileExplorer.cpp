10c65d3d3a3e015af47cb469996ed783c01fb2a5
Zehao Lu <luzehao1998@gmail.com>
2023-04-16 21:28:01 -0700

    move definition to cpp file and change const variable name

---
+++ ToyEngine/UI/View/FileExplorer.cpp
@@ -0,0 +1,58 @@
+#include "UI/View/FileExplorer.h"
+
+namespace ui {
+	FileExplorer::FileExplorer() {
+		int width = 0;
+		int height = 0;
+		int channel = 0;
+		auto fileThumbnail = ToyEngine::StbImageLoader::getImageFrom("file.png", &width, &height, &channel);
+		mFileThumbnailTexture = std::make_shared<ToyEngine::Texture>(fileThumbnail, width, height, GL_RGBA, GL_RGBA, 0, ToyEngine::TextureType::Diffuse);
+
+		auto folderThumbnail = ToyEngine::StbImageLoader::getImageFrom("folder.png", &width, &height, &channel);
+		mFolderThumbnailTexture = std::make_shared<ToyEngine::Texture>(folderThumbnail, width, height, GL_RGBA, GL_RGBA, 0, ToyEngine::TextureType::Diffuse);
+	}
+	void FileExplorer::render() {
+		ImGui::Begin("FileExplorer");
+		float widthAvail = ImGui::GetContentRegionAvail().x;
+
+		// we need to add bracket around std::min to prevent macros expantion
+		int possibleColumnNum = (std::max)((int)(widthAvail / (DEFAULT_THUMBNAIL_PADDING + DEFAULT_THUMBNAIL_WIDTH)), 1);
+
+		if (mCurrentDirectory != mRootDirectory) {
+			if (Button("<-")) {
+				mCurrentDirectory = mCurrentDirectory.parent_path();
+			}
+		}
+
+		ImGui::Columns(possibleColumnNum, 0, false);
+
+		for (auto i : std::filesystem::directory_iterator(mCurrentDirectory)) {
+			if (!i.is_directory()) {
+				GLuint index = mFileThumbnailTexture->getTextureIndex();
+				//https://github.com/ocornut/imgui/issues/4216
+				ImGui::PushStyleColor(ImGuiCol_Button, FILE_BACKGROUND_COLOR);
+				if (ImageButton(i.path().string().c_str(), (void*)(intptr_t)index, { DEFAULT_THUMBNAIL_WIDTH ,DEFAULT_THUMBNAIL_WIDTH }, { 0, 1 }, { 1, 0 }, { 1.0f, 1.0f, 1.0f, 0.0f }, { 1.0f, 1.0f, 1.0f, 1.0f })) {
+				}
+				ImGui::Text(i.path().filename().string().c_str());
+				ImGui::PopStyleColor();
+			}
+			else {
+				GLuint index = mFolderThumbnailTexture->getTextureIndex();
+				ImGui::PushStyleColor(ImGuiCol_Button, FILE_BACKGROUND_COLOR);
+				if (ImageButton(i.path().string().c_str(), (void*)(intptr_t)index, { DEFAULT_THUMBNAIL_WIDTH ,DEFAULT_THUMBNAIL_WIDTH }, { 0, 1 }, { 1, 0 }, { 1.0f, 1.0f, 1.0f, 0.0f }, { 1.0f, 1.0f, 1.0f, 1.0f })) {
+					mCurrentDirectory = i.path();
+					render();
+				}
+				ImGui::Text(i.path().filename().string().c_str());
+				ImGui::PopStyleColor();
+			}
+			ImGui::NextColumn();
+		}
+
+		// We need to set columns to 1 as it will affect other other contents in the window.
+		ImGui::Columns(1);
+
+		ImGui::End();
+	}
+}
+
--- ToyEngine/include/UI/View/FileExplorer.h
+++ ToyEngine/include/UI/View/FileExplorer.h
@@ -20,64 +20,12 @@ using ImGui::ImageButton;
 
 namespace ui {
 	class FileExplorer {
-		const float defaultThumbnailWidth = 128;
-		const float defaultPadding = 30;
+		const float DEFAULT_THUMBNAIL_WIDTH = 128;
+		const float DEFAULT_THUMBNAIL_PADDING = 30;
 	public:
-		FileExplorer() {
-			int width = 0;
-			int height = 0;
-			int channel = 0;
-			auto fileThumbnail = ToyEngine::StbImageLoader::getImageFrom("file.png",&width, &height, &channel);
-			mFileThumbnailTexture = std::make_shared<ToyEngine::Texture>(fileThumbnail, width, height, GL_RGBA, GL_RGBA, 0, ToyEngine::TextureType::Diffuse);
+		FileExplorer();
 
-			auto folderThumbnail = ToyEngine::StbImageLoader::getImageFrom("folder.png", &width, &height, &channel);
-			mFolderThumbnailTexture = std::make_shared<ToyEngine::Texture>(folderThumbnail, width, height, GL_RGBA, GL_RGBA, 0, ToyEngine::TextureType::Diffuse);
-		}
-
-		void render() {
-			ImGui::Begin("FileExplorer");
-			float widthAvail = ImGui::GetContentRegionAvail().x;
-
-			// we need to add bracket around std::min to prevent macros expantion
-			int possibleColumnNum = (std::max)((int)(widthAvail / (defaultPadding + defaultThumbnailWidth)), 1);
-
-			if (mCurrentDirectory != mRootDirectory) {
-				if (Button("<-")) {
-					mCurrentDirectory = mCurrentDirectory.parent_path();
-				}
-			}
-
-			ImGui::Columns(possibleColumnNum, 0, false);
-
-			for (auto i : std::filesystem::directory_iterator(mCurrentDirectory)) {
-				if (!i.is_directory()) {
-					GLuint index = mFileThumbnailTexture->getTextureIndex();
-					//https://github.com/ocornut/imgui/issues/4216
-					ImGui::PushStyleColor(ImGuiCol_Button, FILE_BACKGROUND_COLOR);
-					if (ImageButton(i.path().string().c_str(), (void*)(intptr_t)index, { defaultThumbnailWidth ,defaultThumbnailWidth }, { 0, 1 }, { 1, 0 }, { 1.0f, 1.0f, 1.0f, 0.0f }, { 1.0f, 1.0f, 1.0f, 1.0f })) {
-					}
-					ImGui::Text(i.path().filename().string().c_str());
-					ImGui::PopStyleColor();
-				}
-				else {
-					GLuint index = mFolderThumbnailTexture->getTextureIndex();
-					ImGui::PushStyleColor(ImGuiCol_Button, FILE_BACKGROUND_COLOR);
-					if (ImageButton(i.path().string().c_str(), (void*)(intptr_t)index, { defaultThumbnailWidth ,defaultThumbnailWidth }, {0, 1}, {1, 0}, {1.0f, 1.0f, 1.0f, 0.0f}, {1.0f, 1.0f, 1.0f, 1.0f})) {
-						mCurrentDirectory = i.path();
-						render();
-					}
-					ImGui::Text(i.path().filename().string().c_str());
-					ImGui::PopStyleColor();
-				}
-				ImGui::NextColumn();
-			}
-			
-			// We need to set columns to 1 as it will affect other other contents in the window.
-			ImGui::Columns(1);
-
-			ImGui::End();
-		}
-
+		void render();
 
 	private:
 		const ImVec4 FILE_BACKGROUND_COLOR = ImVec4(1.f, 1.f, 1.f, 0.6f);
