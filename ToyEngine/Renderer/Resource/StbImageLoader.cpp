4811524c229cbd3f9ba534e8867f2e1768af877f
Zehao Lu <luzehao1998@gmail.com>
2023-09-15 12:08:56 -0700

    change image loading default path prefix

--- ToyEngine/Renderer/Resource/StbImageLoader.cpp
+++ ToyEngine/Renderer/Resource/StbImageLoader.cpp
@@ -4,7 +4,7 @@
 
 unsigned char* ToyEngine::StbImageLoader::getImageFrom(std::string path, int* width, int* height, int* channelNum)
 {
-    const std::string DEFAULT_PATH_PREFIX = "Resources/Images/";
+    const std::string DEFAULT_PATH_PREFIX = "";
     stbi_set_flip_vertically_on_load(true);
     unsigned char* data = stbi_load((DEFAULT_PATH_PREFIX + path).c_str(), width, height, channelNum, 0);
 
