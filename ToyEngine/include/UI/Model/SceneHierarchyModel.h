3720522b4938166b5dff689d1572a7254452a3aa
Zehao Lu <luzehao1998@gmail.com>
2023-09-18 12:56:33 -0700

    remove dummy code

--- ToyEngine/include/UI/Model/SceneHierarchyModel.h
+++ ToyEngine/include/UI/Model/SceneHierarchyModel.h
@@ -14,44 +14,6 @@ namespace ui {
 
 	public:
 		SceneHierarchyModel(entt::registry& registry);
-
-		//glm::vec3 getPosition() {
-		//	if (!mSelectedEntity.empty()) {
-		//		return mRegistry.get<ToyEngine::TransformComponent>(mSelectedEntity[0]).worldPos;
-		//	}
-		//	return glm::vec3();
-		//}
-
-		//glm::vec3 getRotation() {
-		//	if (!mSelectedEntity.empty()) {
-		//		return mRegistry.get<ToyEngine::TransformComponent>(mSelectedEntity[0]).rotation_eular;
-		//	}
-		//	return glm::vec3();
-		//}
-
-		//glm::vec3 getScale() {
-		//	if (!mSelectedEntity.empty()) {
-		//		return mRegistry.get<ToyEngine::TransformComponent>(mSelectedEntity[0]).scale;
-		//	}
-		//	return glm::vec3();
-		//}
-
-		//void setPosition(glm::vec3 position) {
-		//	for (auto selected : mSelectedEntity) {
-		//		mRegistry.patch<ToyEngine::TransformComponent>(selected, [position](ToyEngine::TransformComponent& transform) {transform.worldPos = position; });
-		//	}
-		//}
-		//void setRotation(glm::vec3 rotation) {
-		//	for (auto selected : mSelectedEntity) {
-		//		mRegistry.patch<ToyEngine::TransformComponent>(selected, [rotation](ToyEngine::TransformComponent& transform) {transform.rotation_eular = rotation; });
-		//	}
-		//}
-		//void setScale(glm::vec3 scale) {
-		//	for (auto selected : mSelectedEntity) {
-		//		mRegistry.patch<ToyEngine::TransformComponent>(selected, [scale](ToyEngine::TransformComponent& transform) {transform.scale = scale; });
-		//	}
-		//}
-
 		std::vector<std::string> getTagNames() {
 
 			updateDictAndTags();
@@ -68,10 +30,6 @@ namespace ui {
 				mTagToEntityDict[name] = entity;
 			}
 		}
-
-		glm::vec3 mPosition;
-		glm::vec3 mRotation;
-		glm::vec3 mScale;
 
 		std::vector<std::string> mTags;
 		std::unordered_map<std::string, entt::entity> mTagToEntityDict;
